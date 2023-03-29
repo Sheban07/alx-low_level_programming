178f0d75d1f535d8cd392fd93e0a0921a369d3da
root <shebankaingu@gmail.com>
2023-03-29 01:33:53 -0700

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * print_rev - imprime en reversa
- * @: string
+ * @s: string
  * return: 0
  */
 void print_rev(char *s)
 {
 	int longi = 0;
 	int o;
-
 
 	while (*s != '\0')
 	{
